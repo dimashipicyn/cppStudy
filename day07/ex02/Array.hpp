1e5464ebeeeb074b795c266080bec34dd23a7bc8
Lajuana Bespin <lbespin@pr-k6.kzn.21-school.ru>
2021-09-26 18:31:10 +0300

    fix

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -4,25 +4,39 @@
 
 #ifndef CPPSTUDY_ARRAY_HPP
 #define CPPSTUDY_ARRAY_HPP
+#include <iostream>
 
 template <class T>
 class Array {
 public:
-    Array() {
-        len = 0;
-        data = nullptr;
-    }
+    Array() : len_(0), data_(nullptr) {}
     Array(unsigned int n) {
-        data = new T();
-        capacity = n;
+        data_ = new T[n];
+        len_ = n;
     }
-    Array(const Array &array) {
-
+    Array(const Array &array) : len_(0), data_(nullptr) {
+        operator=(array);
     }
+    ~Array() {
+        delete data_;
+    }
+    Array &operator=(const Array &array) {
+        if (data_)
+            delete data_;
+        data_ = new T[array.len_];
+        std::memcpy(data_, array.data_, array.len_ * sizeof(T));
+        len_ = array.len_;
+        return *this;
+    }
+    T &operator[](unsigned int n) {
+        if (n < len_)
+            return data_[n];
+        throw std::out_of_range("Array out of range");
+    }
+    const unsigned int &len() {return len_;}
 private:
-    int len;
-    int capacity;
-    T   *data;
+    unsigned int    len_;
+    T               *data_;
 };
 
 #endif //CPPSTUDY_ARRAY_HPP
